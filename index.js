45c8cca7f251459b146ffdc12706dd5f1bdaad20
Ben Sinclair <ben.d.sinclair@gmail.com>
2013-07-01 20:16:22 +1000

    Support AMD and global variable module styles as well as CommonJS

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
     This library implements purely functional, monadic promises.
 **/
 
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but only CommonJS-like
+        // enviroments that support module.exports, like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+}(this, function () {
+
 /**
     ## `Promise(fork)`
 
@@ -90,7 +104,8 @@ Promise.prototype.map = function(f) {
     });
 };
 
-module.exports = Promise;
+return Promise;
+}));
 
 /**
     ## Fantasy Land Compatible
