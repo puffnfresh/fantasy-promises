5f7d8220983c0ecbef8a5273cbfd953e0ae46eb1
Trevor Basinger <trevor.basinger@gmail.com>
2015-01-26 14:32:06 -0600

    Added transformer

--- test/promise.js
+++ test/promise.js
@@ -1,6 +1,7 @@
 var λ = require('fantasy-check/src/adapters/nodeunit'),
     combinators = require('fantasy-combinators'),
     IO = require('fantasy-io'),
+	I = require ('fantasy-identities'),
     Promise = require('../fantasy-promises'),
 
     fs = require('fs'),
@@ -65,7 +66,28 @@ exports.promise = {
             return promise.extract() === a.toUpperCase();
         },
         [String]
-    )
+    ),
+    'when building transformer, it should return correct value': λ.check(
+		function (a) {
+			var PTI = Promise.PromiseT (I); // Identity Monad
+			return PTI.of (a).fork (identity).x  === I.of (a).x;
+		},
+		[λ.AnyVal]
+    ),
+    'when lifting into transformer, it should return correct value': λ.check(
+		function (a) {
+			var PTI = Promise.PromiseT (I); // Identity Monad
+			return PTI.lift (I.of (a)).fork (identity).x  === I.of (a).x;
+		},
+		[λ.AnyVal]
+    ),
+    'when chaining transformer, it should return correct value': λ.check(
+		function (a) {
+			var PTI = Promise.PromiseT (I); // Identity Monad
+			return PTI.of (a).chain (PTI.lift).fork (identity).x  === I.of (a).x;
+		},
+		[λ.AnyVal]
+    ),
 };
 
 exports.testReadFile = function(test) {
